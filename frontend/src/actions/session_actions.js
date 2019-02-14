a1d535c1b7f2d4ebc0f7ee00415eacde9089ff1d
Corey Ladovsky <coreyskymusic@gmail.com>
2019-02-14 11:22:13 -0500

    session actions

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -1,10 +1,24 @@
 import * as SessionApiUtil from '../util/session_api_util';
-
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+export const NEXT_PAGE = "NEXT_PAGE";
+export const CLEAR_PAGE = "ClEAR_PAGE";
+
 
 export const receiveCurrentUser = currentUser => ({
   type: RECEIVE_CURRENT_USER,
   currentUser
+});
+
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const clearErrors = () => ({
+  type: CLEAR_ERRORS
 });
 
 export const getUser = () => dispatch =>{
@@ -14,6 +28,21 @@ export const getUser = () => dispatch =>{
     }
   )
   .catch(err => {
-    debugger
+    return dispatch(receiveErrors(err.response))
   })
 }
+
+export const login = user => dispatch =>
+  SessionApiUtil.login(user).then(
+    usr => dispatch(receiveCurrentUser(usr)),
+    errors => dispatch(receiveErrors(errors.response))
+  );
+
+export const logout = () => dispatch =>
+  SessionApiUtil.logout().then(() => dispatch(receiveCurrentUser(null)));
+
+export const signup = user => dispatch =>
+  SessionApiUtil.signUp(user).then(
+    usr => dispatch(receiveCurrentUser(usr)),
+    errors => dispatch(receiveErrors(errors.response))
+  );
--- frontend/src/util/session_api_util.js
+++ frontend/src/util/session_api_util.js
@@ -1,3 +1,9 @@
 import axios from 'axios';
 
 export const getUser = () => axios.get('/auth/getUser')
+
+export const signUp = user => axios.post('/auth/new', user)
+
+export const login = user => axios.post('/auth/login', user)
+
+export const logout = () => axios.get('/auth/logout')
