44b49adea35442ca7c7fd3056eee27f06470e603
Corey Ladovsky <coreyskymusic@gmail.com>
2019-02-14 13:05:38 -0500

    css starting on sign up

--- frontend/src/components/auth/SignUp.js
+++ frontend/src/components/auth/SignUp.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import { Link } from 'react-router-dom';
+import logo from "../../assets/logo.png"
+import '../../css/SignUp.css';
 class SignUp extends React.Component {
   state = { email: "", password: "", age: "" };
 
@@ -15,12 +17,21 @@ class SignUp extends React.Component {
     let {email, password, age} = this.state;
     return (
       <div className="SignUp">
+        <Link to="/login" >  <button  className="Login"> Log in </button></Link>
+      <div className="SignUp-Background">
+        <div><img src={logo} alt="logo" /> </div>
+          <h1>Sign up to see more</h1>
+          <div>
+          Access dinterest's best ideas with a free account
+          </div>
         <form onSubmit={this.handleSubmit}>
           <input id="email" type="text" placeholder="Email" value={email} onChange={this.handleChange} />
           <input id="password" type="password" placeholder="Create a password" value={password} onChange={this.handleChange}  />
           <input id="age" type="text" placeholder="Age" value={age} onChange={this.handleChange}  />
           <button type="submit">Sign Up</button>
         </form>
+
+        </div>
       </div>
     );
   }
