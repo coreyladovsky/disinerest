fc1696b5fef2867e99f4a1e3b33a820dba438771
Corey Ladovsky <coreyskymusic@gmail.com>
2019-02-25 16:40:40 -0500

    pin show close

--- frontend/src/components/pins/PinsShow.js
+++ frontend/src/components/pins/PinsShow.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../../css/PinsShow.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import defaultImage from "../../assets/defaultUsere.jpg";
+import "../../css/PinsShow.css";
 
 class PinsShow extends React.Component {
   state = { hoverPhotos: false };
@@ -10,36 +11,76 @@ class PinsShow extends React.Component {
 
   showPinSaverModal = e => {
     e.stopPropagation();
-  }
+  };
 
   componentDidMount() {
     window.scrollTo(0, 0);
-    this.props.fetchPin(this.props.match.params.id)
+    this.props.fetchPin(this.props.match.params.id);
   }
-  render () {
+
+  goBack = e => {
+    this.props.history.goBack();
+  };
+  render() {
     let { pin, currentUser } = this.props;
-    if(!pin) return null;
-    if(!currentUser) return null;
+    if (!pin) return null;
+    if (!currentUser) return null;
     return (
       <div className="PinsShow">
-        <button className="show-back-button"><i class="fa fa-chevron-left" aria-hidden="true"></i>Back</button>
+        <button className="show-back-button" onClick={this.goBack.bind(this)}>
+          <i class="fa fa-chevron-left" aria-hidden="true" />
+          Back
+        </button>
         <div className="PinsDisplayBox">
-          <div className="image-holder"  >
+          <div className="image-holder">
             <div className="pinControl">
+              {pin.user_id === currentUser.id ? (
+                <Link to={"/pins/" + pin.id + "/edit"}>
+                  {" "}
+                  <div className="PinsShowEditButton">
+                    <i class="fa fa-pencil" aria-hidden="true" />
+                  </div>
+                </Link>
+              ) : (
+                <div />
+              )}
 
-          {pin.user_id === currentUser.id ? <Link to={"/pins/" + pin.id + "/edit"}> <div className="PinsShowEditButton"><i class="fa fa-pencil" aria-hidden="true"></i></div></Link> : <div></div>}
-
-              <button className="show-pinCreate-modal" onClick={this.showPinSaverModal}>
+              <button
+                className="show-pinCreate-modal"
+                onClick={this.showPinSaverModal}
+              >
                 {" "}
-                <i class="fa fa-thumb-tack dontChange" aria-hidden="true" /> Save
+                <i
+                  class="fa fa-thumb-tack dontChange"
+                  aria-hidden="true"
+                />{" "}
+                Save
               </button>
             </div>
 
-          <img src={pin.image_url} onMouseOver={this.toggleHover} />
-        <div className={this.state.hoverPhotos ? "whiteImageCover" : "whiteImageCoverHide" }
-          onMouseOut={this.toggleHover.bind(this)}>
+            <img className="picShowCover" src={pin.image_url} onMouseOver={this.toggleHover} />
+            <div
+              className={
+                this.state.hoverPhotos
+                  ? "whiteImageCover"
+                  : "whiteImageCoverHide"
+              }
+              onMouseOut={this.toggleHover.bind(this)}
+            />
+            <a className="pinLinkTag" href={pin.link_url} target="_blank">
+              <i className="fa fa-arrow-up" aria-hidden="true" /> {pin.link_url}
+            </a>
 
-          </div>
+            <div className="pinOwnerInfo">
+              <div className="displayInfo">
+                <img
+                  className="diplayInfoUserPic"
+                  src={pin.owner_image ? pin.owner_image : defaultImage}
+                  alt="pin owner"
+                />
+              <div>{pin.owner_email ? pin.owner_email.split("@")[0]: "Unknown"}</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
